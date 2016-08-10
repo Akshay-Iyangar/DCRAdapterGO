23b6963e31c3b0ecd29115bbe07e0c31fd0fdd18
Akshay Iyangar <aiyangar@lmdv-aiyangar.jomax.paholdings.com>
2016-08-09 18:02:47 -0700

    correct url for dashboard

--- hostname.sh
+++ hostname.sh
@@ -4,7 +4,7 @@
 
 HOST_IP=$(curl http://rancher-metadata/2015-12-19/self/host/agent_ip)
 
-RESTurl="dashboard.ecom.int.godaddy.com/"
+RESTurl="http://dashboard.ecom.int.godaddy.com/"
 
 DCRurl=$(echo "http://$HOST_IP:12285/v1/dc/logs/ecomm/logs")
 
